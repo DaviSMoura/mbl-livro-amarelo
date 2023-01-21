8e78840d6fa555bfa552439e89d6a0b86e6743d1
Davi Moura <davisilvag4mer@gmail.com>
2023-01-21 03:21:01 -0300

    feat: create theme for livroamarelo

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui')],
+  separator: "_",
+  daisyui: {
+    themes: [
+      {
+        livroamarelo: {
+          primary: "#29373A",
+          secondary: "#000000",
+          accent: "#E5AE41",
+          neutral: "#A43814",
+          "base-100": "#29373A",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 }
