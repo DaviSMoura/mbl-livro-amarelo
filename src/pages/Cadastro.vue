282bdde3449aa4111536b5d444695d67c62b8f79
Davi Moura <davisilvag4mer@gmail.com>
2023-01-24 22:13:49 -0300

    feat: validation

--- src/pages/Cadastro.vue
+++ src/pages/Cadastro.vue
@@ -1,31 +1,70 @@
 <script setup>
 import { ref } from 'vue'
+import { useForm, useField } from 'vee-validate'
+import * as yup from 'yup'
 
 const webhookUrl = atob(`aHR0cHM6Ly93b3JrZmxvdy5kYXZpbW91cmEuY29tLmJyL3dlYmhvb2svbWJsL2xpdnJvYW1hcmVsbw==`)
-const form = ref({
-  nome: '',
-  email: '',
-  telefone: '',
-  sexo: 'Masculino',
-  cidade: '',
-  estado: 'SP',
-  escolaridade: 'Ensino Médio incompleto',
-  areaAtuacaoProfissional: '',
-  expGestaoPublica: '',
-  curriculo: '',
-  areaContribuicao: 'Selecione',
-  horasSemana: 'Selecione'
+
+// max file size is 10MB
+const MAX_FILE_SIZE = 10 * 1024 * 1024
+const fileTestYup = {
+  message: `Arquivo muito grande. Máximo ${MAX_FILE_SIZE / 1024 / 1024}MB`,
+  test: (file) => {
+    if (!file) return true
+    const isValid = file?.size < MAX_FILE_SIZE;
+    return isValid;
+  }
+}
+
+yup.setLocale({
+  mixed: {
+    required: 'Campo obrigatório'
+  }
 })
+const validationSchema = yup.object({
+  nome: yup.string().required(),
+  email: yup.string().email('Email inválido').required(),
+  telefone: yup.string().required(),
+  sexo: yup.string().required(),
+  cidade: yup.string().required(),
+  estado: yup.string().required(),
+  escolaridade: yup.string().required().notOneOf(['Selecione'], 'Selecione uma opção'),
+  areaAtuacaoProfissional: yup.string().required(),
+  expGestaoPublica: yup.string().required(),
+  curriculo: yup.mixed().required('Selecione um arquivo').test(fileTestYup),
+  areaContribuicao: yup.string().required().notOneOf(['Selecione'], 'Selecione uma opção'),
+  horasSemana: yup.string().required().notOneOf(['Selecione'], 'Selecione uma opção'),
+})
+const { errors, handleSubmit } = useForm({ validationSchema, initialValues: {
+  sexo: 'Masculino',
+  estado: 'SP',
+  escolaridade: 'Selecione',
+  areaContribuicao: 'Selecione',
+  horasSemana: 'Selecione',
+}})
+const { value: nome } = useField('nome')
+const { value: email } = useField('email')
+const { value: telefone } = useField('telefone')
+const { value: sexo } = useField('sexo')
+const { value: cidade } = useField('cidade')
+const { value: estado } = useField('estado')
+const { value: escolaridade } = useField('escolaridade')
+const { value: areaAtuacaoProfissional } = useField('areaAtuacaoProfissional')
+const { value: expGestaoPublica } = useField('expGestaoPublica')
+const { value: curriculo } = useField('curriculo')
+const { value: areaContribuicao } = useField('areaContribuicao')
+const { value: horasSemana } = useField('horasSemana')
 
 const loading = ref(false)
 const showSuccess = ref(false)
 
-const submit = async () => {
+const submit = handleSubmit(async values => {
   loading.value = true
+  console.log('submited', values)
 
   const formData = new FormData()
-  Object.keys(form.value).forEach(key => {
-    formData.append(key, form.value[key])
+  Object.keys(values).forEach(key => {
+    formData.append(key, values[key])
   })
 
   let response
@@ -44,12 +83,12 @@ const submit = async () => {
     alert('Erro ao realizar cadastro! Tente mais tarde.')
   }
   loading.value = false
-}
+})
 
 
 const handleFileChange = async function ($event) {
   const file = $event.target.files[0]
-  form.value.curriculo = file
+  curriculo.value = file
 }
 </script>
 
@@ -59,28 +98,33 @@ const handleFileChange = async function ($event) {
   .py-10.front-header.header-content
     h1.heading.text-3xl.md_text-4xl.mx-auto.flex.justify-center CADASTRO
     img.front-header.flex.justify-center.py-10.header-hero-image.mx-auto(src="/src/assets/brazil-map.svg")
-.py-10.container.mx-auto.px-5.form-container
+form.py-10.container.mx-auto.px-5.form-container(@submit="submit")
   .py-3
     h3.text-xl.accent-text.text-shadow.mb-2 NOME COMPLETO
-    input.input.input-bordered.input-sm.w-full.md_input-md(type="text" placeholder="Ex. Kim Kataguiri" v-model="form.nome")
+    input.input.input-bordered.input-sm.w-full.md_input-md(type="text" placeholder="Ex. Kim Kataguiri" v-model.trim="nome")
+    .text-red-500.text-sm.mt-1(v-if="errors.nome") {{ errors.nome }}
   .py-3
     h3.text-xl.accent-text.text-shadow.mb-2 EMAIL DE CONTATO
-    input.input.input-bordered.input-sm.w-full.md_input-md(type="text" placeholder="Ex. suporte@mbl.org.br" v-model="form.email")
+    input.input.input-bordered.input-sm.w-full.md_input-md(type="text" placeholder="Ex. suporte@mbl.org.br" v-model="email")
+    .text-red-500.text-sm.mt-1(v-if="errors.email") {{ errors.email }}
   .py-3
     h3.text-xl.accent-text.text-shadow.mb-2 TELEFONE DE CONTATO
-    input.input.input-bordered.input-sm.w-full.md_input-md(type="text" v-mask-phone.br placeholder="Ex. (11) 91234-1234" v-model="form.telefone")
+    input.input.input-bordered.input-sm.w-full.md_input-md(type="text" v-mask-phone.br placeholder="Ex. (11) 91234-1234" v-model="telefone")
+    .text-red-500.text-sm.mt-1(v-if="errors.telefone") {{ errors.telefone }}
   .py-3
     h3.text-xl.accent-text.text-shadow.mb-2 SEXO
-    select.select.select-sm.select-bordered.w-full.md_input-md(v-model="form.sexo")
+    select.select.select-sm.select-bordered.w-full.md_input-md(v-model="sexo")
       option Masculino
       option Feminino
+    .text-red-500.text-sm.mt-1(v-if="errors.sexo") {{ errors.sexo }}
   .py-3.gap-4.grid.grid-cols-3
     div.col-span-2
       h3.text-xl.accent-text.text-shadow.mb-2 CIDADE
-      input.input.input-bordered.input-sm.w-full.md_input-md(type="text" placeholder="Ex. São Paulo" v-model="form.cidade")
+      input.input.input-bordered.input-sm.w-full.md_input-md(type="text" placeholder="Ex. São Paulo" v-model="cidade")
+      .text-red-500.text-sm.mt-1(v-if="errors.cidade") {{ errors.cidade }}
     div
       h3.text-xl.accent-text.text-shadow.mb-2 ESTADO
-      select.select.select-sm.select-bordered.w-full.md_select-md(v-model="form.estado")
+      select.select.select-sm.select-bordered.w-full.md_select-md(v-model="estado")
         option AC
         option AL
         option AP
@@ -108,9 +152,11 @@ const handleFileChange = async function ($event) {
         option SP
         option SE
         option TO
+      .text-red-500.text-sm.mt-1(v-if="errors.estado") {{ errors.estado }}
   .py-3
     h3.text-xl.accent-text.text-shadow.mb-2 ESCOLARIDADE
-    select.select.select-sm.select-bordered.w-full.md_select-md(v-model="form.escolaridade")
+    select.select.select-sm.select-bordered.w-full.md_select-md(v-model="escolaridade")
+      option(selected disabled) Selecione
       option Ensino Médio incompleto
       option Ensino Médio completo
       option Graduação incompleto
@@ -118,19 +164,24 @@ const handleFileChange = async function ($event) {
       option Pós-graduação
       option Mestrado
       option Doutorado
+      option Pós-doutorado
+    .text-red-500.text-sm.mt-1(v-if="errors.escolaridade") {{ errors.escolaridade }}
   .py-3
     h3.text-xl.accent-text.text-shadow.mb-2 ÁREA DE ATUAÇÃO PROFISSIONAL
-    input.input.input-bordered.input-sm.w-full.md_input-md(type="text" placeholder="Ex. Economia" v-model="form.areaAtuacaoProfissional")
+    input.input.input-bordered.input-sm.w-full.md_input-md(type="text" placeholder="Ex. Economia" v-model="areaAtuacaoProfissional")
+    .text-red-500.text-sm.mt-1(v-if="errors.areaAtuacaoProfissional") {{ errors.areaAtuacaoProfissional }}
   .py-3
     h3.text-xl.accent-text.text-shadow.mb-2 EXPERIÊNCIA EM GESTÃO PÚBLICA
-    input.input.input-bordered.input-sm.w-full.md_input-md(type="text" placeholder="Ex. Nenhuma" v-model="form.expGestaoPublica")
+    input.input.input-bordered.input-sm.w-full.md_input-md(type="text" placeholder="Ex. Nenhuma" v-model="expGestaoPublica")
+    .text-red-500.text-sm.mt-1(v-if="errors.expGestaoPublica") {{ errors.expGestaoPublica }}
   .py-3
     h3.text-xl.accent-text.text-shadow.mb-2 CURRICULO VITAE/LATTES (PDF)
     input.file-input.file-input-bordered.file-input-sm.w-full.md_file-input-md(type="file" @change="handleFileChange" accept="application/pdf")
+    .text-red-500.text-sm.mt-1(v-if="errors.curriculo") {{ errors.curriculo }}
   .py-3
     h3.text-xl.accent-text.text-shadow EM QUE ÁREA VOCÊ PRETENDE CONTRIBUIR COM O LIVRO AMARELO?
     h3.accent-text.text-shadow.mb-2 *A área de contribuição deve convergir com sua área de atuação acadêmica/profissional.
-    select.select.select-sm.select-bordered.w-full.md_select-md(v-model="form.areaContribuicao")
+    select.select.select-sm.select-bordered.w-full.md_select-md(v-model="areaContribuicao")
       option(disabled selected) Selecione
       option Economia
       option Saúde
@@ -147,21 +198,24 @@ const handleFileChange = async function ($event) {
       option Justiça
       option Redes Sociais e Liberdade de Imprensa
       option Social
+      option Educação
+    .text-red-500.text-sm.mt-1(v-if="errors.areaContribuicao") {{ errors.areaContribuicao }}
   .py-3
     h3.text-xl.accent-text.text-shadow.mb-2 QUANTAS HORAS POR SEMANA VOCÊ PODE DEDICAR Á PESQUISA ESCRITA DO LIVRO AMARELO?
-    select.select.select-sm.select-bordered.w-full.md_select-md(v-model="form.horasSemana")
+    select.select.select-sm.select-bordered.w-full.md_select-md(v-model="horasSemana")
       option(disabled selected) Selecione
       option 1 hora por semana
       option 2-3 horas por semana
       option 3-5 horas por semana
       option + de 5 horas por semana
+    .text-red-500.text-sm.mt-1(v-if="errors.horasSemana") {{ errors.horasSemana }}
   .alert.alert-success.shadow-lg(v-if="showSuccess")
     div
       svg.stroke-current.flex-shrink-0.h-6.w-6(xmlns='http://www.w3.org/2000/svg' fill='none' viewbox='0 0 24 24')
         path(stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z')
       span Enviado com sucesso!
   .container.justify-center.flex(class="py-[40px] md_py-[50px]")
-    button.btn.btn-wide.btn-accent.md_btn-lg(:class="{loading}" @click.stop="submit") Participar
+    button.btn.btn-wide.btn-accent.md_btn-lg(:class="{loading}" type="submit" value="Participar") Participar
   .container.py-20.justify-center.flex
     img(src="/src/assets/mbl-gold.svg" class="md_w-1/6 w-1/4")
 </template>
